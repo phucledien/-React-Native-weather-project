93da7beeeae1a06927ad19b70a80f9013d4fbd5e
Phuc Le Dien <phucledien@gmail.com>
2018-05-19 12:15:07 +0700

    Added weather api

--- App.js
+++ App.js
@@ -6,33 +6,52 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Forecast from "./components/Forecast";
 
-type Props = {};
-export default class App extends Component<Props> {
+import OpenWeatherMap from "./lib/open_weather_map"
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { zip: "", forecast: null };
+  }
+
+  _handleTextChange = event => {
+    let zip = event.nativeEvent.text;
+    OpenWeatherMap.fetchForecast(zip)
+      .then(forecast => {
+        console.log(forecast);
+        this.setState({ forecast });
+      });
+    this.setState({zip: event.nativeEvent.text})
+  }
+
   render() {
+    let content = null;
+    if (this.state.forecast !== null) {
+      content = (
+        <Forecast
+          main={this.state.forecast.main}
+          description={this.state.forecast.description}
+          temp={this.state.forecast.temp}/>
+      )
+    }
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          You input {this.state.zip}.
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        {content}
+        <TextInput
+          style={styles.input}
+          onSubmitEditing={this._handleTextChange}/>
       </View>
     );
   }
@@ -43,16 +62,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#666666',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  input: {
+    fontSize: 20,
+    borderWidth: 2,
+    height: 40,
+    width: 100,
+    textAlign: "center",
+  }
 });
